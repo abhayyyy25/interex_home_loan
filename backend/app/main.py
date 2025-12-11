bf81e24fd284312c56c1973c3dbf1333616b4525
abhayyyy25 <abhay.gfuturetech@gmail.com>
2025-12-11 14:37:31 +0530

    Update backend main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,8 @@ app.add_middleware(
         "http://localhost:5000",
         "http://localhost:5173",
         "http://127.0.0.1:5000",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://interex-home-loan-frontend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
